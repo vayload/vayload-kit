9bf6a0310ecdda7633089e0c6bc2bdb4bd786566
Zweiter <asegundo442@gmail.com>
2026-03-04 12:15:54 -0500

    fix bin path

--- tests/cli_tests.rs
+++ tests/cli_tests.rs
@@ -1,26 +1,27 @@
 #[cfg(test)]
 mod tests {
+    const BIN: &str = "target/debug/vk";
     use assert_cmd::prelude::*;
     use predicates::prelude::*;
     use std::process::Command;
 
     #[test]
     fn test_help_command() {
-        let mut cmd = Command::new("vk");
+        let mut cmd = Command::new(BIN);
         cmd.arg("--help");
         cmd.assert().success().stdout(predicate::str::contains("Vayload Kit (vk)"));
     }
 
     #[test]
     fn test_version_command() {
-        let mut cmd = Command::new("vk");
+        let mut cmd = Command::new(BIN);
         cmd.arg("--version");
         cmd.assert().success().stdout(predicate::str::contains(env!("CARGO_PKG_VERSION")));
     }
 
     #[test]
     fn test_install_without_manifest_fails() {
-        let mut cmd = Command::new("vk");
+        let mut cmd = Command::new(BIN);
         cmd.arg("install");
         cmd.assert().failure();
     }
